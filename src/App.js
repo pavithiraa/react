6a1fa480d71ed5fd844dda8f86628e738dbf9d59
pavithiraa <hrpavithraa2000@gmail.com>
2021-12-25 22:24:57 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plans/>
+
     </div>
   );
 }
+function Plans(){
+  const plans=[
+    {name:'FREE',
+    price:0,
+    features:[
+     " Single User",
+     "5GB Storage",
+     "Unlimited Public Projects",
+     "Community Access",
+     "Unlimited Private Projects",
+     "Dedicated Phone Support",
+     "Free Subdomains",
+     "Monthly Status Reports" ]},
+    {name:'PLUS',
+    price:9,
+    features:[
+     "5 Users",
+     "50GB Storage",
+     "Unlimited Public Projects",
+     "Community Access",
+     "Unlimited Private Projects",
+     "Dedicated Phone Support",
+     "Free Subdomains",
+     "Monthly Status Reports"
+    ]
+  },
+    {name:'PRO',
+    price:49,
+    features:[
+     "Unlimited Users",
+     "150GB Storage",
+     "Unlimited Public Projects",
+     "Community Access",
+     "Unlimited Private Projects",
+     "Dedicated Phone Support",
+     "Unlimited Free Subdomains",
+     "Monthly Status Reports"
+    ]}
+  ]
+  return(<div>
+    {plans.map((plan)=>(
+    <Card name={plan.name} price={plan.price} features={plan.features}/>
+    ))}
+  </div>)
+}
+function Card(props){
+  return(
+    <div className='card'>
+      <p className='plan-name'>{props.name}</p>
+      <p className='price'>$ {props.price}<span className='price-duration'>/month</span></p>
+      <ul className='features'>
+        {props.features.map(feature =><li>{feature}</li> )}
+      </ul>
+      <button className='buy'>BUY</button>
+
+    </div>
+  )
+}
 
 export default App;
+
